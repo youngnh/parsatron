5da5fbfee3c0b0c57ecaed783d2add4331f35bfb
Nate Young <nyoung@revelytix.com>
2011-02-05 11:48:23 -0600

    initial project documentation

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject parsatron "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject parsatron "0.0.1-SNAPSHOT"
+  :description "Clojure parser combinators"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
