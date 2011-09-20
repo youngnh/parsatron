ddee90d8bf92f0094496052e9c7e30f5b91d6c84
Nate Young <nyoung@revelytix.com>
2011-09-20 07:56:44 -0500

    bumped to 0.0.2-SNAPSHOT, removed monolithic contrib dependency

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject the/parsatron "0.0.1"
+(defproject the/parsatron "0.0.2-SNAPSHOT"
   :description "Clojure parser combinators"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
