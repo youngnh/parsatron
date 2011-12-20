d0cda9b5c3f01e3585d1622775e890141bf7b57d
Nate Young <nyoung@revelytix.com>
2011-12-20 08:40:17 -0600

    0.0.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject the/parsatron "0.0.2-SNAPSHOT"
+(defproject the/parsatron "0.0.2"
   :description "Clojure parser combinators"
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
