338aaa62dff065c72e0dcaf5277309d3c15628c9
Nate Young <nyoung@revelytix.com>
2013-01-14 10:34:55 -0600

    0.0.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject the/parsatron "0.0.4"
+(defproject the/parsatron "0.0.5-SNAPSHOT"
   :description "Clojure parser combinators"
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies [[swank-clojure "1.4.2"]]
