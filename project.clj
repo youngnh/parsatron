65382aa6cc92623edfc0f9d09d026a60cfd103ba
Joel Holdbrooks <cjholdbrooks@gmail.com>
2014-05-24 18:46:12 -0700

    Add run-tests alias

--- project.clj
+++ project.clj
@@ -16,6 +16,12 @@
   :source-paths ["src" "target/generated-src/clj" "target/generated-src/cljs"]
   :test-paths ["src" "target/generated-test"]
 
+  :aliases
+  {"run-tests" ["do"
+                "test,"
+                "cljx" "once,"
+                "cljsbuild" "test"]}
+
   :cljx
   {:builds [{:source-paths ["src/"]
              :output-path "target/generated-src/clj"
@@ -33,6 +39,9 @@
   :cljsbuild
   {:builds [{:source-paths ["target/generated-src/cljs" "target/generated-test"]
              :compiler {:output-to "target/cljs/testable.js"
+                        ;; Automatically run the ClojureScript test
+                        ;; suite on compile.
+                        :notify-command ["phantomjs" :cljs.test/runner "target/cljs/testable.js"]
                         :optimizations :simple
                         :pretty-print true}}]
    :test-commands {"unit-tests" ["phantomjs" :cljs.test/runner "target/cljs/testable.js"]}}
