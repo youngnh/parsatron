f32570d21c6735761711f4423e501d5926802d44
Paula Gearon <pgearon@cisco.com>
2018-08-07 16:34:18 -0400

    Updated Clojure, ClojureScript, and lein-cljsbuild. Added test target and compiler options for ClojureScript, though :simple optimizations are still having the error in https://dev.clojure.org/jira/browse/CLJS-101

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject the/parsatron "0.0.8-SNAPSHOT"
   :description "Clojure parser combinators"
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2227"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.339"]]
 
-  :plugins [[lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
 
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/clj"]
@@ -12,4 +12,7 @@
   :global-vars {*warn-on-reflection* false}
 
   :cljsbuild {:builds [{:source-paths ["src/cljs" "test/cljs"]
-                        :compiler {:output-to "test/resources/parsatron_test.js"}}]})
+                        :compiler {:optimizations :simple
+                                   :target :nodejs
+                                   :output-to "test/resources/parsatron_test.js"}}]
+              :test-commands { "unit" ["node" "test/resources/parsatron_test.js"]}})
