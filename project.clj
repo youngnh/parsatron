d51338ac0f0039a9d98652d0f22401de177ec887
Paula Gearon <pgearon@cisco.com>
2020-01-08 21:12:53 -0500

    Updated project paths, bumped dependencies, and split ClojureScript dev and test environments

--- project.clj
+++ project.clj
@@ -2,17 +2,24 @@
   :description "Clojure parser combinators. Forked from Nate Young's Master"
 
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"]]
+                 [org.clojure/clojurescript "1.10.597"]]
 
   :plugins [[lein-cljsbuild "1.1.7"]]
 
-  :source-paths ["src/clj" "src/cljs"]
-  :test-paths ["test/clj"]
+  :source-paths ["src"]
+  :test-paths ["test"]
 
   :global-vars {*warn-on-reflection* false}
 
-  :cljsbuild {:builds [{:source-paths ["src/cljs" "test/cljs"]
-                        :compiler {:optimizations :simple
-                                   :target :nodejs
-                                   :output-to "test/resources/parsatron_test.js"}}]
+  :cljsbuild {:builds
+                {:dev
+                 {:source-paths ["src"]
+                  :compiler {:optimizations :simple
+                             :target :nodejs
+                             :output-to "target/parsatron.js"}}
+                 :test
+                 {:source-paths ["src" "test"]
+                  :compiler {:optimizations :simple
+                             :target :nodejs
+                             :output-to "test/resources/parsatron_test.js"}} }
               :test-commands { "unit" ["node" "test/resources/parsatron_test.js"]}})
