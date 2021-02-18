f7559cf57702482708361f1b1b6d6bdbec26aa2f
Paula Gearon <pgearon@cisco.com>
2021-02-18 14:12:07 -0500

    Cleaned up whitespace

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject org.clojars.quoll/parsatron "0.0.10"
   :description "Clojure parser combinators"
 
-  :dependencies [[org.clojure/clojure "1.10.2"] ]
+  :dependencies [[org.clojure/clojure "1.10.2"]]
 
   :plugins [[lein-cljsbuild "1.1.8"]]
 
@@ -14,15 +14,16 @@
     :dev {
       :dependencies [[org.clojure/clojurescript "1.10.773"]]}}
 
-  :cljsbuild {:builds
-                {:dev
-                 {:source-paths ["src"]
-                  :compiler {:optimizations :simple
-                             :target :nodejs
-                             :output-to "target/parsatron.js"}}
-                 :test
-                 {:source-paths ["src" "test"]
-                  :compiler {:optimizations :simple
-                             :target :nodejs
-                             :output-to "test/resources/parsatron_test.js"}} }
-              :test-commands { "unit" ["node" "test/resources/parsatron_test.js"]}})
+  :cljsbuild {
+    :builds {
+      :dev {
+        :source-paths ["src"]
+        :compiler {:optimizations :simple
+                   :target :nodejs
+                   :output-to "target/parsatron.js"}}
+      :test {
+        :source-paths ["src" "test"]
+        :compiler {:optimizations :simple
+                   :target :nodejs
+                   :output-to "test/resources/parsatron_test.js"}} }
+    :test-commands {"unit" ["node" "test/resources/parsatron_test.js"]}})
